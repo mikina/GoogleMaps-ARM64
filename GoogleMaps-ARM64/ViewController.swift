c5841d570c565a2ee035886e7ff2e41045b710b9
Mike Mikina <mike@mikina.pl>
2021-05-30 15:50:19 +0200

    Original framework provided by Google.

--- GoogleMaps-ARM64/ViewController.swift
+++ GoogleMaps-ARM64/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let camera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: 2)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
     }
 
 
